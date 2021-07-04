c00db3b7e6af85d58cf521a7f7f3bf4477aead61
Adisok <adisok@wp.pl>
2021-07-04 17:39:58 +0200

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,21 +1,22 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page # Widok, ktory uzyjemy zaraz, przechowywany w folderze lists w pliku views.py
 
 
 class SmokeTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/') # Resolve odpowiada za powiazanie URL do Widoku,   
-		self.assertEqual(found.func, home_page) #"/" - oznacza home_page,
-
 	def test_home_page_returns_correct_html(self):
-		request = HttpRequest() # tworzymy requesta, czyli to Django zobaczy jak uzytkownik zapyta sie przegladary o strone
-		response = home_page(request) # Przekazujemy requesta do naszego widoku co da na odpowiedz (instancja klasy HttpResponse)
-		html = response.content.decode('utf8') # dobieramy sie do zawartosci i konwertujemy na htmla
-		self.assertTrue(html.startswith('<html>')) # sprawdzamy poczatkowy tag 
-		self.assertIn('<title>To-Do lists</title>', html) # sprawdzamy czy w tytule jest 'To-Do lists'
-		self.assertTrue(html.endswith('</html>')) # sprawdzamy koncowy tag
-		
+		response = self.client.get('/') #podajem klientowi URL ktore chcemy przetestowac
+		self.assertTemplateUsed(response, 'home.html') # od TestCase, pozwala na sprawdzenie,
+													   # ktora templatka zostala uzyta do wyrenderowania odpowiedzi,
+													   # bedzie tylko dzialac dla odpowiedzi zwroconych przez test clienta
+
+		# request = HttpRequest() # tworzymy requesta, czyli to Django zobaczy jak uzytkownik zapyta sie przegladary o strone
+		# response = home_page(request) # Przekazujemy requesta do naszego widoku co da na odpowiedz (instancja klasy HttpResponse)
+		# html = response.content.decode('utf8') # zostawiamy stare testy tylko po to zeby sprawdzic czy jest git
+		# self.assertTrue(html.startswith('<html>'))
+		# self.assertIn('<title>To-Do lists</title>', html)
+		# self.assertTrue(html.strip().endswith('</html>'))
