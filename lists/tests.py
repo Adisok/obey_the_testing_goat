983b57b02168c6654061a081ed393f81164282c8
Adisok <adisok@wp.pl>
2021-07-04 13:51:43 +0200

    First unit test

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page # Widok, ktory uzyjemy zaraz, przechowywany w folderze lists w pliku views.py
 
 class SmokeTest(TestCase):
 
-	def test_bad_maths(self):
-		self.assertEqual(1+1, 3)
-		
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/') # Resolve odpowiada za powiazanie URL do Widoku,   
+		self.assertEqual(found.func, home_page) #"/" - oznacza home_page,
+
