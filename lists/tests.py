2cd0c52a3dceb1d989d1f70c2ea8e70d4c80e55b
Adisok <adisok@wp.pl>
2021-07-04 20:03:35 +0200

    Last commit before massage

--- lists/tests.py
+++ lists/tests.py
@@ -8,7 +8,7 @@ from lists.views import home_page # Widok, ktory uzyjemy zaraz, przechowywany w 
 
 class SmokeTest(TestCase):
 
-	def test_home_page_returns_correct_html(self):
+	def test_uses_home_template(self):
 		response = self.client.get('/') #podajem klientowi URL ktore chcemy przetestowac
 		self.assertTemplateUsed(response, 'home.html') # od TestCase, pozwala na sprawdzenie,
 													   # ktora templatka zostala uzyta do wyrenderowania odpowiedzi,
@@ -20,3 +20,8 @@ class SmokeTest(TestCase):
 		# self.assertTrue(html.startswith('<html>'))
 		# self.assertIn('<title>To-Do lists</title>', html)
 		# self.assertTrue(html.strip().endswith('</html>'))
+
+	def test_can_save_a_POST_request(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
+		self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	return render(request, 'home.html')
+	# if request.method == 'POST':
+	# 	return render(request, 'home.html', {
+	# 			'new_item_text': request.POST['item_text'],
+	# 		})
+	return render(request, 'home.html',{
+	 			'new_item_text': request.POST.get('item_text',''), 
+	 			# dla defaultowej wartosci (czyli dla geta)uzywamy dict.get
+	 		})
