9e5256277410076597aea9c92ba0e65e34c1b5ba
Adam Sokolowski <adam.sokolowski@yougov.com>
2021-10-13 10:14:10 +0200

    Add test for checking that every user have unique url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+
+MAX_WAIT = 10 #maksymalny czas czekania na glicze
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -14,10 +18,23 @@ class NewVisitorTest(LiveServerTestCase):
 	# Edith has heard about a cool new online to-do app. She goes
 	# to check out its homepage
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+					#Lapiemy wyjatki, WebDriverException, czyli gdy strona sie nie zaladowala,
+					# i Selenium nie moze znalezc elementu na stronie,
+					# AssertionError, gdy tabela jest na stronie ale pewnie jest to przed odswiezeniem,
+					# wiec nie ma naszych wierszy
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get(self.live_server_url)
@@ -33,36 +50,83 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),  
 			'Enter a to-do item'
-			)
-		
+			)		
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
 		inputbox.send_keys('Buy peacock feathers') # wpisywanie do elementow typu input
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER) # klasa Keys pozwala na wysylanie specialnych klawiszy jak enter
-		time.sleep(1) # czekamy zeby strona zdazyla sie zaladowac
+
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly') # wpisywanie do elementow typu input
 		inputbox.send_keys(Keys.ENTER) # klasa Keys pozwala na wysylanie specialnych klawiszy jak enter
-		time.sleep(1) # czekamy zeby strona zdazyla sie zaladowac
-		
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-				
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr') 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		self.fail('Finish the tests!')
 		# The page updates again, and now shows both items on her list
+	
 
+		# Satisfied, she goes back to sleep
+
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+		#Edith starts a new to-do list
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+		#She notices that her list has unique URL
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
+		self.fail('Finish the tests!')
 
 		# She visits that URL - her to-do list is still there.
 
-		# Satisfied, she goes back to sleep
+
+		# Now a new user, Francis, comes along to the site.
+		## We use a new browser session to make sure that no information
+		## of Edith's is through from cookies etc
+
+		self.browser.quit()
+		self.browser = webdriver.Safari()
+
+		## Francis visits the home page. There is no sign of Edith's
+		# list 
+
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text 
+		self.assertNotIn('Buy peacock feathers', page_next)
+		self.assertNotIn('make a fly', page_next)
+
+
+		# Francis starts a new list by entering a new item. 
+		# He is less interesting than Edith
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		self.wait_for_row_in_list_table('1: Buy milk')
+
+		#Francis gets his own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(edith_list_url, francis_list_url)
+
+		# Again, there is no trace of Edith's list
+		page_text = self.browser.find_element_by_tag_name('body').row_text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_next)
+
+		# Satisfied, they both go back to sleep
+
+
+
