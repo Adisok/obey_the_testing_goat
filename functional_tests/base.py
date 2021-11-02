f70afa7e8f2e04b9b18b62d3044577e76a23aeff
Adam Sokolowski <adam.sokolowski@yougov.com>
2021-11-02 22:28:49 +0100

    Move Fts into their own individual files

---
+++ functional_tests/base.py
@@ -0,0 +1,41 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.common.exceptions import WebDriverException
+from selenium import webdriver
+import time
+import os
+
+MAX_WAIT = 10 #maksymalny czas czekania na glicze
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Safari()
+		staging_server = os.environ.get("STAGING_SERVER")
+		if staging_server:
+			self.live_server_url = "http://" + staging_server
+		
+	def tearDown(self):
+		self.browser.refresh()
+		self.browser.quit()	
+	
+	# Edith has heard about a cool new online to-do app. She goes
+	# to check out its homepage
+
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+					#Lapiemy wyjatki, WebDriverException, czyli gdy strona sie nie zaladowala,
+					# i Selenium nie moze znalezc elementu na stronie,
+					# AssertionError, gdy tabela jest na stronie ale pewnie jest to przed odswiezeniem,
+					# wiec nie ma naszych wierszy
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
+
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,45 +1,12 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
+from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-import time
-import os
+from selenium import webdriver
 
 MAX_WAIT = 10 #maksymalny czas czekania na glicze
 
-class NewVisitorTest(StaticLiveServerTestCase):
+class NewVisitorTest(FunctionalTest):
 
-	def setUp(self):
-		self.browser = webdriver.Safari()
-		staging_server = os.environ.get("STAGING_SERVER")
-		if staging_server:
-			self.live_server_url = "http://" + staging_server
-		
-	def tearDown(self):
-		self.browser.refresh()
-		self.browser.quit()	
 	
-	# Edith has heard about a cool new online to-do app. She goes
-	# to check out its homepage
-
-	def wait_for_row_in_list_table(self, row_text):
-		start_time = time.time()
-		while True:
-			try:
-				table = self.browser.find_element_by_id('id_list_table')
-				
-				rows = table.find_elements_by_tag_name('tr')
-				self.assertIn(row_text, [row.text for row in rows])
-				return
-			except (AssertionError, WebDriverException) as e:
-					#Lapiemy wyjatki, WebDriverException, czyli gdy strona sie nie zaladowala,
-					# i Selenium nie moze znalezc elementu na stronie,
-					# AssertionError, gdy tabela jest na stronie ale pewnie jest to przed odswiezeniem,
-					# wiec nie ma naszych wierszy
-				if time.time() - start_time > MAX_WAIT:
-					raise e
-				time.sleep(0.5)
-
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get(self.live_server_url)
 
@@ -129,30 +96,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertIn('Buy milk', page_text)
 
 		# Satisfied, they both go back to sleep
-
-
-	def test_layout_and_styling(self):
-		# Edith goes to the home page
-		self.browser.get(self.live_server_url)
-		self.browser.set_window_size(1024, 768)
-
-		# She notices that inpub box is nicely centered
-		inputbox = self.browser.find_element_by_id("id_new_item")
-		self.assertAlmostEqual(
-				inputbox.location["x"] + inputbox.size["width"] / 2,
-				512,
-				delta=10
-			)
-
-		# She starts a new list and ses the input is nicely centered there too
-
-		inputbox.send_keys("testing")
-		inputbox.send_keys(Keys.ENTER)
-		self.wait_for_row_in_list_table("1: testing")
-		inputbox = self.browser.find_element_by_id("id_new_item")
-		self.assertAlmostEqual(
-			inputbox.location["x"] + inputbox.size["width"] / 2,
-			512,
-			delta=10,
-			)
-
